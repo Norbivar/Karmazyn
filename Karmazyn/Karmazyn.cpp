185a0be813cfc4a827ea2c572135ac7f1b65aa08
Norbert Varjú <norbivar96@gmail.com>
2019-02-21 17:36:25 +0100

    [1] Initializing Git repository

---
+++ Karmazyn/Karmazyn.cpp
@@ -0,0 +1,16 @@
+﻿// Karmazyn.cpp : Defines the entry point for the application.
+//
+
+#include "Karmazyn.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	while (true)
+	{
+
+	}
+	return 0;
+}
