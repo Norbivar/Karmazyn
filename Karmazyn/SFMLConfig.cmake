2c9b5db6f2c8e387345a93f3266d4a5eb4421b24
Norbert Varjú <norbivar96@gmail.com>
2019-02-22 17:11:00 +0100

    [2] Very basic SFML include/lib find through ENV

---
+++ Karmazyn/SFMLConfig.cmake
@@ -0,0 +1,26 @@
+#set(FIND_SFML_PATHS
+#    "${CMAKE_CURRENT_LIST_DIR}/../.."
+#    ${SFML_ROOT}
+#    $ENV{SFML_ROOT}
+#    ~/Library/Frameworks
+#    /Library/Frameworks
+#    /usr/local
+#    /usr
+#    /sw
+#    /opt/local
+#    /opt/csw
+#    /opt)
+
+#find_path(SFML_ROOT_DIR SFML.tag
+#          PATHS ${FIND_SFML_PATHS})
+
+	if (EXISTS "$ENV{SFML_ROOT}/include")
+		set(SFML_INCLUDE_DIRS "$ENV{SFML_ROOT}/include/")
+	else()
+		 message(FATAL_ERROR "Could not find SFML include dir!")
+	endif()
+	if (EXISTS "$ENV{SFML_ROOT}/lib")
+		set(SFML_LIBRARIES "$ENV{SFML_ROOT}/lib/")
+	else()
+		 message(FATAL_ERROR "Could not find SFML lib dir!")
+	endif()
