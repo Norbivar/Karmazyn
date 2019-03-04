5a097c24f7146be58e77199c5f07d2d92a87af46
Norbert Varjú <norbivar96@gmail.com>
2019-03-04 10:34:57 +0100

    [3] Disabled console logging in release mode

--- Libs/Logger.hpp
+++ Libs/Logger.hpp
@@ -11,8 +11,10 @@ namespace Karmazyn
 
 		static Logger* GetLogger();
 		Logger() :
-			RotatedTxtLogger  (spdlog::rotating_logger_mt("txtlogger", LoggerSettings::cLogFile, 1024 * 1024 * 20, 3)),
-			ConsoleLogger     (spdlog::stdout_color_mt("console"))
+			RotatedTxtLogger  (spdlog::rotating_logger_mt("txtlogger", LoggerSettings::cLogFile, 1024 * 1024 * 20, 3))
+		#ifndef NDEBUG
+			, ConsoleLogger   (spdlog::stdout_color_mt("console"))
+		#endif
 		{
 			spdlog::set_async_mode(8192);
 			spdlog::set_pattern("[%H:%M:%S] %v");
@@ -30,46 +32,60 @@ namespace Karmazyn
 		inline void info(const char *fmt, const Arg1 &arg1, const Args &... args)
 		{
 			RotatedTxtLogger->info(fmt, arg1, args...);
+		#ifndef NDEBUG
 			ConsoleLogger->info(fmt, arg1, args...);
+		#endif
 		}
 
 		template<typename Arg1, typename... Args>
 		inline void warn(const char *fmt, const Arg1 &arg1, const Args &... args)
 		{
 			RotatedTxtLogger->warn(fmt, arg1, args...);
+		#ifndef NDEBUG
 			ConsoleLogger->warn(fmt, arg1, args...);
+		#endif
 		}
 
 		template<typename Arg1, typename... Args>
 		inline void error(const char *fmt, const Arg1 &arg1, const Args &... args)
 		{
 			RotatedTxtLogger->error(fmt, arg1, args...);
+		#ifndef NDEBUG
 			ConsoleLogger->error(fmt, arg1, args...);
+		#endif
 		}
 
 		template<typename T>
 		inline void info(const T& msg)
 		{
 			RotatedTxtLogger->info(msg);
+		#ifndef NDEBUG
 			ConsoleLogger->info(msg);
+		#endif
 		}
 
 		template<typename T>
 		inline void warn(const T& msg)
 		{
 			RotatedTxtLogger->warn(msg);
+		#ifndef NDEBUG
 			ConsoleLogger->warn(msg);
+		#endif
 		}
 
 		template<typename T>
 		inline void error(const T& msg)
 		{
 			RotatedTxtLogger->error(msg);
+		#ifndef NDEBUG
 			ConsoleLogger->error(msg);
+		#endif
 		}
 
 	private:
 		std::shared_ptr<spdlog::logger> RotatedTxtLogger;
+	#ifndef NDEBUG
 		std::shared_ptr<spdlog::logger> ConsoleLogger;
+	#endif
 	};
 }
