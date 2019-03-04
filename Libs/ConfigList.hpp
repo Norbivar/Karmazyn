c512e633065b715a19f3da40f89c32466791c9a3
Norbert Varjú <norbivar96@gmail.com>
2019-03-04 10:41:39 +0100

    [4] Config Reader leftover (constant changes)

--- Libs/ConfigList.hpp
+++ Libs/ConfigList.hpp
@@ -1,7 +1,7 @@
 namespace Configs
 {
-	constexpr const char* const Root_Graphics = "GRAPHICS";
-		constexpr const char* const Graphics_Render_X = "render_x";
-		constexpr const char* const Graphics_Render_Y = "render_y";
-		constexpr const char* const Graphics_Frame_Cap = "maxfps";
+	// Please, only use Config lookups with these. TODO: find something that is strongly-typed and constexpr, to enforce it
+	constexpr const char* const ResolutionX = "render_x";
+	constexpr const char* const ResolutionY = "render_y";
+	constexpr const char* const FrameRateCap = "maxfps";
 }
