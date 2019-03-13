3bf737612b7fe124e33f8ff8936e0ad4e76be5b4
Norbert Varjú <norbivar96@gmail.com>
2019-03-13 11:58:18 +0100

    [7] Basic Asset Map/Cache for holding textures

---
+++ Karmazyn/AssetManager/AssetManager.cpp
@@ -0,0 +1,35 @@
+#include "AssetManager.hpp"
+#include <SFML/Graphics.hpp>
+
+namespace Karmazyn
+{
+	template<typename AssetType>
+	size_t AssetContainer<AssetType>::onRequestFlushUnused()
+	{
+		size_t deleted { 0 };
+		for (auto it = m_AssetContainer.begin(); it != m_AssetContainer.end(); ++it)
+		{
+			if (it->second.use_count() == 1)
+			{
+				m_AssetContainer.erase(it);
+				++deleted;
+			}
+		}
+		return deleted;
+	}
+
+
+	
+	AssetManager::AssetManager()
+	{
+
+	}
+
+	size_t AssetManager::signalUnusedCacheFlush()
+	{
+		return
+			m_TextureAssets.onRequestFlushUnused()
+
+			;
+	}
+}
---
+++ Karmazyn/AssetManager/AssetManager.hpp
@@ -0,0 +1,61 @@
+#pragma once
+#include <memory>
+#include <map>
+#include <string>
+
+#include <SFML/Graphics.hpp>
+
+namespace Karmazyn
+{
+	// Asset must have a "loadFromFile" method.
+	template<typename AssetType>
+	class AssetContainer
+	{
+		friend class AssetManager;
+		AssetContainer() {};
+
+		std::shared_ptr<AssetType> acquire(const std::string& asset_file_name) // TODO: make threadsafe, as the potential for fucking up is huge
+		{
+			const auto AssetIterator = m_AssetContainer.find(asset_file_name);
+			if (AssetIterator != m_AssetContainer.end())
+			{
+				return AssetIterator->second;
+			}
+			else
+			{
+				std::shared_ptr<AssetType> asset = std::make_shared<AssetType>();
+				asset->loadFromFile(asset_file_name);
+				m_AssetContainer.emplace(asset_file_name, asset);
+				return asset;
+			}
+			return nullptr;
+		}
+
+		size_t onRequestFlushUnused();
+
+		std::map< std::string, std::shared_ptr<AssetType> > m_AssetContainer;
+	};
+
+	class AssetManager
+	{
+	public:
+		AssetManager();
+
+		template<typename T>
+		std::shared_ptr<T> acquire(const std::string& asset_file_name)
+		{
+			static_assert(false, "AssetManager::acquire instantiated with wrong type!");
+		}
+
+		template<>
+		std::shared_ptr<sf::Texture> acquire<sf::Texture>(const std::string& asset_file_name)
+		{
+			return m_TextureAssets.acquire(asset_file_name);
+		}
+
+		size_t signalUnusedCacheFlush();
+
+	private:
+		AssetContainer<sf::Texture> m_TextureAssets;
+	};
+}
